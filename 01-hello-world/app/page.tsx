8c5864c29fc922f095afa45ca091ee8abca3caad
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-10-02 21:15:19 +0500

    first-nextjs-hello-world

---
+++ 01-hello-world/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-center mt-[200px]'>
+     <h1 className='text-5xl font-bold uppercase'> hello world</h1>
+     <p className='text-2xl mt-5'>Here is my First Nextjs Hello World</p>
+    </div>
+  )
+}
+
+export default page
