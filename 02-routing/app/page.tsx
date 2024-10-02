fcc946c804edbc6ec9c3ee02cd15b7e316e5c24a
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-10-02 21:31:32 +0500

    nextjs-routing

---
+++ 02-routing/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+     <div className='flex justify-center items-center'>
+      <h1 className='text-4xl font-bold uppercase'>Home</h1>
+     </div>
+    </>
+  )
+}
+
+export default page
+
